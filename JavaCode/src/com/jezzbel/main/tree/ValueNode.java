7c73dbc8ff99fc7f8090397088d2ef5e253377b3
AgataM <jezzbel@toya.net.pl>
2014-03-03 14:48:09 +0100

    removed comment

--- JavaCode/src/com/jezzbel/main/tree/ValueNode.java
+++ JavaCode/src/com/jezzbel/main/tree/ValueNode.java
@@ -1,11 +1,10 @@
 package com.jezzbel.main.tree;
 
-/**
- * Created by Lucy on 21.02.14.
- */
+
 public class ValueNode extends Node {
     private double value;
 
+    /////
     public ValueNode(double value) {
         this.value = value;
     }
