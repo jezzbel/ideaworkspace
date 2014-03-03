27917258501d8bd9f40754f11611e5bc1dfe0d26
jezzbel <jezzbel@toya.net.pl>
2014-03-03 14:49:35 +0100

    Update ValueNode
    removed comment

--- JavaCode/src/com/jezzbel/main/tree/ValueNode.java
+++ JavaCode/src/com/jezzbel/main/tree/ValueNode.java
@@ -4,7 +4,7 @@ package com.jezzbel.main.tree;
 public class ValueNode extends Node {
     private double value;
 
-    /////
+   
     public ValueNode(double value) {
         this.value = value;
     }
